e4dcfbf0e564d4940191f8b60fe949114686eda9
SuperFunkyNinja <68922959+SuperFunkyNinja@users.noreply.github.com>
2021-11-23 16:15:55 +0000

    update

--- DocumentCompiler_016867.py
+++ DocumentCompiler_016867.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from tkinter.messagebox import showerror
 
 EXCEL = Path(
-    r"C:\EPDMVault\Projects\Current\016867s L&T Hydrocarbon Saudi\04 - Final Manual Books\MRB Books\016867 - MRB index.xlsx"
+    r"C:\EPDMVault\Projects\Current\016867s L&T Hydrocarbon Saudi\04 - Final Manual Books\MRB Books\016867 - Purchased MRB index.xlsx"
 )
 
 # Set working directory to excel file location
